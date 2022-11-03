7f47e9d6bd556584cdc259e1d5d27516f423b56b
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2022-11-02 23:41:18 -0700

    add get all route for voters

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const db = require('../../db/database')
+const inputCheck= require('../../utils/inputCheck')
 router.use(require('./candidateRoutes'));
+
+
+router.get('/voters', (req, res)=>{
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
+    const params = []
+    db.all(sql, params, (err, rows)=>{
+        if(err){
+            res.status(500).json({ error: err.message})
+        }
+        res.json({
+            message: "SUCCESS!!!",
+            data: rows
+        })
+    })
+})
+
 
 module.exports = router;
