c3287d1016d2363e2689d05d323fd4a9ceb0d619
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2022-11-02 21:48:29 -0700

    create query for parties

--- server.js
+++ server.js
@@ -17,6 +17,51 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.')
 });
 
+// parties querys
+
+app.get('/api/parties', (req, res)=>{
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows)=>{
+        if(err){
+            res.status(500).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'SUCCESS!!',
+            data: rows
+        });
+
+    })
+});
+
+app.get('/api/party/:id', (req, res)=>{
+    const sql = `SELECT * FROM parties WHERE id =?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row)=>{
+        if(err){
+            res.status(400).json({ error: err.message})
+            return;
+        };
+        res.json({
+            message: 'SUCCESS!!',
+            data: row
+        })
+    })
+})
+
+app.delete('/api/party/:id', (req, res)=>{
+    const sql = `DELETE FROM parties WHERE id=?`;
+    const params = [req.params.id]
+    db.run(sql, params, function(err, result){
+        if(err){
+            res.status(400).json({ error: err.message});
+                return;
+        }
+        res.json({ message:'successfully deleted', changes: this.changes})
+        })
+    });
+
 //request all the info on the table
 app.get('/api/candidates', (req, res)=>{
     const sql = `SELECT candidates.*, parties.name
